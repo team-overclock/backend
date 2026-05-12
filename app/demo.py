2aaf1db306a5e6e1edcac8d163abd7d3986d3f18
kimzuni <me@kimzuni.com>
2026-05-13 00:58:26 +0900

    feat: 데모용 게스트 로그인 및 엔드포인트 추가
    - main.py에서 demo 관련 삭제 코드 시 관련 기능이 모두 제거됨
    - app/demo.py 파일 추가 및 `create_guest_user` 함수 위치 이동
    - 게스트 계정 정보를 환경변수로 관리하도록 변경

---
+++ app/demo.py
@@ -0,0 +1,68 @@
+"""데모용 함수 모음"""
+
+import os
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, Request, status
+from sqlalchemy.orm import Session
+
+from .database import get_db
+from .core import session
+from .core.exception import AppException
+from .crud.user import create_user
+from .schemas.auth import UserCreateRequest
+from .schemas.user import UserInfo
+
+
+load_dotenv()
+
+GUEST_LOGIN_ENABLE = (os.getenv("GUEST_LOGIN_ENABLE") or "false").lower() == "true"
+GUEST_EMAIL = os.getenv("GUEST_EMAIL") or "guest@example.com"
+GUEST_USERNAME = os.getenv("GUEST_USERNAME") or GUEST_EMAIL.split("@")[0]
+GUEST_PASSWORD = os.getenv("GUEST_PASSWORD") or "guest"
+
+
+# fns
+
+def create_guest_user(db: Session):
+    """
+    게스트 사용자 생성
+    """
+
+    if not GUEST_LOGIN_ENABLE:
+        raise AppException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            message="현재 게스트 로그인 기능이 비활성화 상태입니다.",
+        )
+
+    return create_user(db, UserCreateRequest(
+        name=GUEST_USERNAME,
+        email=GUEST_EMAIL,
+        password=GUEST_PASSWORD,
+    ))
+
+def get_guest_user(db: Session):
+    """
+    게스트 사용자 조회
+    """
+
+    user, _ = create_guest_user(db)
+    return user
+
+
+# router
+
+router = APIRouter(tags=["demo"])
+
+@router.post(
+    "/auth/guest",
+    status_code=status.HTTP_200_OK,
+    tags=["auth"],
+)
+def guest_login(
+    request: Request,
+    db: Session = Depends(get_db),
+) -> UserInfo:
+    """게스트 로그인 처리 함수"""
+    user = get_guest_user(db)
+    session.login(request, user)
+    return user
